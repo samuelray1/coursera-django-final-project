3278c75cd25477af2c722a334fbc603f9a7dd4d5
samuelray1 <samuelraymond900@gmail.com>
2024-03-18 04:55:46 -0400

    update file

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -120,7 +120,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
-    is_correcr = models.BooleanField(default=False)
+    is_correct = models.BooleanField(default=False)
 
 class Submission(models.Model):
    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
